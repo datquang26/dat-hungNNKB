63ebca75a8742b1abcdaaa855beda74d1a646b75
duy <taquangdat2266@gmail.com>
2023-11-13 18:05:47 +0700

    pushAll

---
+++ src/route/web.js
@@ -0,0 +1,29 @@
+import express from "express";
+import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+    router.get("/", homeController.getHomePage);
+
+    router.get("/crud", homeController.getCRUD); // Tạo
+
+    router.post("/post-crud", homeController.postCRUD); // Trang khi tạo xong
+
+    router.get('/get-crud', homeController.displayGetCRUD); // Hiện thị
+
+    router.get('/edit-crud', homeController.getEditCRUD); // Sửa
+
+    router.post('/put-crud', homeController.putCRUD); // Trang khi sửa xog
+
+    router.get('/delete-crud', homeController.deleteCRUD); // Xóa
+
+    router.post('/api/login', userController.handleLogin);
+
+    router.get('/api/get-all-users', userController.handleGetAllUsers);
+
+    return app.use("/", router);
+}
+
+module.exports = initWebRoutes;
