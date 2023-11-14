9a61242f5b31c1b374327550e3f0522b4f946f21
duy <taquangdat2266@gmail.com>
2023-11-14 11:53:08 +0700

    vd42

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -26,7 +26,7 @@ let handleLogin = async (req, res) => {
 }
 
 let handleGetAllUsers = async (req, res) => {
-    let id = req.body.id;
+    let id = req.query.id;
 
     if (!id) {
         return res.status(200).json({
@@ -44,8 +44,33 @@ let handleGetAllUsers = async (req, res) => {
     })
 }
 
+let handleCreateNewUser = async (req, res) => {
+    let message = await userService.createNewUser(req.body);
+    return res.status(200).json(message);
+}
+
+let handleDeleteUser = async (req, res) => {
+    if(!req.body.id) {
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: 'Mising required parameters!'
+        })
+    }
+    let message = await userService.deleteUser(req.body.id);
+    return res.status(200).json(message);
+}
+
+let handleEditUser = async (req, res) => {
+    let data = req.body;
+    let message = await userService.updateUserData(data);
+    return res.status(200).json(message)
+}
+
 
 module.exports = {
     handleLogin: handleLogin,
-    handleGetAllUsers: handleGetAllUsers
+    handleGetAllUsers: handleGetAllUsers,
+    handleCreateNewUser: handleCreateNewUser,
+    handleDeleteUser: handleDeleteUser,
+    handleEditUser: handleEditUser,
 }
