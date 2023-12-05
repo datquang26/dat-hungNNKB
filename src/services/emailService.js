16f26a43ab4dfdf18f5c1ad19fe11f4c8ec90c75
duy <taquangdat2266@gmail.com>
2023-12-05 09:25:06 +0700

    vd88

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -17,23 +17,52 @@ let sendSimpleEmail = async (dataSend) => {
         to: dataSend.reciverEmail,
         subject:"Thông tin đặt lịch khám bệnh",
         // text: "Hello word?",
-        html: `
-                    <h3>Xin chào ${dataSend.patientName}</h3>
-                    <p>Bạn nhận được email này vì bạn đã đặt lịch khám bệnh online trên DAT&HUNG </p>
-                    <p>Thông tin đặt lịch khám bệnh:</p>
-                    <div>Thời Gian: ${dataSend.time}</div>
-                    <div><b>Bác sĩ: ${dataSend.doctorName}</b></div>
-                    <p>Nếu các thông tin trên là đúng, vui lòng click vào đường link bên dưới để xác nhận và hoàn tất thủ tục đặt lịch khám bệnh</p>
-                    <div>
-                        <a href=${dataSend.redirectLink} target="_blank">Click here </a>
-                    </div>
-                    <div>
-                        Xin chân thành cảm ơn
-                    </div>
-            `,
+        html: getBodyHTMLEmail(dataSend),
     });
 }
 
+
+let getBodyHTMLEmail = (dataSend) => {
+    let result = '';
+    if(dataSend.language === 'vi'){
+        result = 
+        `
+        <h3>Xin chào ${dataSend.patientName}</h3>
+        <p>Bạn nhận được email này vì bạn đã đặt lịch khám bệnh online trên DAT&HUNG </p>
+        <p>Thông tin đặt lịch khám bệnh:</p>
+        <div>Thời Gian: ${dataSend.time}</div>
+        <div><b>Bác sĩ: ${dataSend.doctorName}</b></div>
+        <p>Nếu các thông tin trên là đúng, vui lòng click vào đường link bên dưới để xác nhận và hoàn tất thủ tục đặt lịch khám bệnh</p>
+        <div>
+            <a href=${dataSend.redirectLink} target="_blank">Click here </a>
+        </div>
+        <div>
+            Xin chân thành cảm ơn
+        </div>
+        `
+
+    }
+    if(dataSend.language === 'en'){
+        result = 
+        `
+        <h3>Dear ${dataSend.patientName}</h3>
+        <p>You received this email because you made an online medical appointment on DAT&HUNG</p>
+        <p>Information for scheduling medical examination:</p>
+        <div>Time: ${dataSend.time}</div>
+        <div><b>Doctor: ${dataSend.doctorName}</b></div>
+        <p>If the above information is correct, please click on the link below to confirm and complete the medical appointment booking procedure.</p>
+        <div>
+            <a href=${dataSend.redirectLink} target="_blank">Click here </a>
+        </div>
+        <div>
+           Sincerely thank !
+        </div>
+        `
+
+    }
+
+    return result;
+}
 
 
 module.exports = {
--- src/services/patientServices.js
+++ src/services/patientServices.js
@@ -1,24 +1,34 @@
 
 import db from "../models/index";
 require('dotenv').config();
-import emailService from './emailService'
+import emailService from './emailService';
+import { v4 as uuidv4 } from 'uuid';
+
+let buildUrlEmail = (doctorId, token) => {
+    let result = `${process.env.URL_REACT}/verify-booking?token=${token}&doctorId=${doctorId}`
+    return result;
+}
 
 let postBookAppointment = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!data.email || !data.doctorId || !data.timeType || !data.date) {
+            if (!data.email || !data.doctorId || !data.timeType || !data.date
+                || !data.fullName
+                ) {
                 resolve ({
                     errCode: 1,
                     errMessage: 'Missing parameter'
                 })
             } else {
 
+                let token = uuidv4();
                 await emailService.sendSimpleEmail({
                     reciverEmail:data.email,
-                    patientName: "Đạt & Hùng patient name",
-                    time: '8:00-9:00 Thứ Hai 4/12/2023',
-                    doctorName: "Đạt Hùng",
-                    redirectLink: 'https://www.facebook.com/taquangdat2605/'
+                    patientName:data.fullName,
+                    time: data.timeString,
+                    doctorName: data.doctorName,
+                    language: data.language,
+                    redirectLink: buildUrlEmail(data.doctorId, token)
                 })
 
 
@@ -39,7 +49,8 @@ let postBookAppointment = (data) => {
                             doctorId: data.doctorId,
                             patientId: user[0].id,
                             date: data.date,
-                            timeType: data.timeType
+                            timeType: data.timeType,
+                            token: token
                         }
                     })
                 }
@@ -55,6 +66,45 @@ let postBookAppointment = (data) => {
     })
 }
 
+let postVerifyBookAppointment = (data) => {
+    return new Promise( async(resolve, reject) => {
+        try {
+            if(!data.token || !data.doctorId) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing parameter'
+                })
+            } else {
+                let appointment = await db.Booking.findOne({
+                    where: {
+                        doctorId: data.doctorId,
+                        token: data.token,
+                        statusId: 'S1'
+                    },
+                    raw: false
+                })
+                if (appointment) {
+                    appointment.statusId = 'S2';
+                    await appointment.save();
+
+                    resolve({
+                        errCode: 0,
+                        errMessage: "Update the appointment succeed!"
+                    })
+                } else {
+                    resolve({
+                        errCode: 2,
+                        errMessage:"Appointment has been activated or does not exist"
+                    })
+                }
+            }
+        } catch (e) {
+            reject(e);
+        }
+    } )
+}
+
 module.exports = {
-    postBookAppointment: postBookAppointment
+    postBookAppointment: postBookAppointment,
+    postVerifyBookAppointment: postVerifyBookAppointment
 }
